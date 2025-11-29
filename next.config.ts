4decd461b8e04f30177eaead435f0cf02797d734
Aryan Kumar <aryanak9163@gmail.com>
2025-11-29 12:05:10 +0530

    updates the prisma config ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    reactCompiler: true,
+    reactCompiler: true, turbopack: {
+        resolveAlias: {
+            '@prisma/client': './src/generated/prisma',
+        }
+    }
 };
 
 export default nextConfig;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,23 @@
-import "dotenv/config";
+import {config} from  "dotenv";
 import { defineConfig, env } from "prisma/config";
+import path from "node:path";
+
+config({
+    path: path.resolve(__dirname, ".",".env.local"),
+})
+
+const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl) {
+    throw new Error(`Missing DB URL: ${dbUrl}`);
+}
 
 export default defineConfig({
-    schema: "prisma/schema.prisma",
+    schema: "./prisma/schema.prisma",
     migrations: {
-        path: "prisma/migrations",
-        seed: "tsx prisma/db-seed.ts"
+        path: path.join(process.cwd(), "prisma", "migrations"),
+        seed: "pnpm exec tsx prisma/db-seed.ts"
     },
     datasource: {
-        url: process.env.DATABASE_URL!
+        url: dbUrl
     }
 });
